00be323334e771462ff0f9336dc363c359d4dbc3
Sahil Mehta <sahilbmehta290303@gmail.com>
2023-03-20 10:55:58 +0530

    cmt

--- main.py
+++ main.py
@@ -1,40 +1,38 @@
 import streamlit as st
-import yfinance as yf
 import pandas as pd
-import cufflinks as cf
-import datetime
+from PIL import Image
+import yfinance as yf
+
+st.write("""
+#Stock Market Web Application
+Visually show data on a stock! Date range Jan 1,2010 to Dec 31,2022
+""")
+
+start = '2010-01-01'
+end = '2022-12-31'
+
+image = Image.open('C:/Users/91886/PycharmProjects/stockPredictor/venv/image1.jpg')
+st.image(image, use_column_width = True)
+
+st.sidebar.header('User Input')
+def get_input():
+    start_date = st.sidebar.text_input("Start Date", start)
+    end_date = st.sidebar.text_input("End Date", end)
+    stock_symbol = st.sidebar.text_input("stock_symbol")
+    return start_date, end_date, stock_symbol
+
+start1, end1, tick = get_input()
+
+df = yf.download(tick, start1, end1)
+
+st.header("Close Price\n")
+st.line_chart(df['Close'])
+
+st.header("Volume\n")
+st.line_chart(df['Volume'])
+
+st.header("Data Statistics\n")
+st.write(df.describe())
 
 
-st.write('---')
 
-# Sidebar
-st.sidebar.subheader('Query parameters')
-start_date = st.sidebar.date_input("Start date", datetime.date(2019, 1, 1))
-end_date = st.sidebar.date_input("End date", datetime.date(2021, 1, 31))
-
-# Retrieving tickers data
-ticker_list = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/s-and-p-500-companies/master/data/constituents_symbols.txt')
-tickerSymbol = st.sidebar.selectbox('Stock ticker', ticker_list) # Select ticker symbol
-tickerData = yf.Ticker(tickerSymbol) # Get ticker data
-tickerDf = tickerData.history(period='1d', start=start_date, end=end_date)
-
-# Ticker information
-string_logo = '<img src=%s>' % tickerData.info['logo_url']
-st.markdown(string_logo, unsafe_allow_html=True)
-
-string_name = tickerData.info['longName']
-st.header('**%s**' % string_name)
-
-string_summary = tickerData.info['longBusinessSummary']
-st.info(string_summary)
-
-# Ticker data
-st.header('**Ticker data**')
-st.write(tickerDf)
-
-# Bollinger bands
-st.header('**Bollinger Bands**')
-qf = cf.QuantFig(tickerDf,title='First Quant Figure',legend='top',name='GS')
-qf.add_bollinger_bands()
-fig = qf.iplot(asFigure=True)
-st.plotly_chart(fig)
